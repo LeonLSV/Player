173c092f164cdfc7196e565c575228620a01f204
Ramiro Contreras León <racramiro@gmail.com>
2020-10-19 22:39:51 -0500

    scroll album

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,12 +3,19 @@ import "./styles/AlbumList.css";
 import AlbumItem from "./AlbumItem";
 import turizo2 from "../images/turizo2.jpg";
 import turizo4 from "../images/turizo4.jpg";
+import turizo from "../images/turizo.jpg";
+import turizo1 from "../images/turizo1.jpg";
+import turizo3 from "../images/turizo3.jpg";
 
 class ContentAlbum extends React.Component {
   render() {
     return (
-      <div className="albumitem__container">
+      <div className="albumitem__container scroll-album">
         <AlbumItem image={turizo2} />
+        <AlbumItem image={turizo4} />
+        <AlbumItem image={turizo} />
+        <AlbumItem image={turizo1} />
+        <AlbumItem image={turizo3} />
         <AlbumItem image={turizo4} />
       </div>
     );
